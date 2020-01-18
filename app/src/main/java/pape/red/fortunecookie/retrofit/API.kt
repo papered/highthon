1f2c4884a292d3b8768bec8bb5af2d3324ffb957
LeeJongHyun <dwg7691@gmail.com>
2020-01-19 01:02:59 +0900

    Add network config

--- app/src/main/java/pape/red/fortunecookie/retrofit/API.kt
+++ app/src/main/java/pape/red/fortunecookie/retrofit/API.kt
@@ -1,3 +1,14 @@
 package pape.red.fortunecookie.retrofit
 
-interface API
+import okhttp3.MultipartBody
+import retrofit2.Response
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface Api {
+
+    @Multipart
+    @POST("/article/image")
+    suspend fun createArticle(@Part file: MultipartBody.Part): Response<Unit>
+}
